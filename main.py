70b86bd46ab392750a37829378fbde67d8ef4ec9
Luana <20201ti40053@ifba.edu.br>
2026-01-22 20:20:58 -0300

    Update main.py

--- main.py
+++ main.py
@@ -13,5 +13,19 @@ class ToDo:
         self.db = Database()
         self.db.criar_tabela()
         
-        self.db.criar_painel_principal()
+        self.db.painel_principal()
+    
+    def painel_principal(self):
+         
+         #limpar a tela principal
+        for widget in self.root.winfo_children():
+            widget.destroy()  
+        
+        tk.label(
+            self.root,
+            text="Minhas Tarefas",
+            font=("Arial", 20, "bold", "italic"),
+            bg="ffffff",
+            fg="333333"
+        ).pack(pady=20)   
     
