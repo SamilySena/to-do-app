5e018b167ca61a11bf08e85e71af1425fd7889ed
Luana <20201ti40053@ifba.edu.br>
2026-01-22 20:14:01 -0300

    finalmente

--- main.py
+++ main.py
@@ -6,12 +6,27 @@ from create_database import Database
 class ToDo:
     def __init__(self, root):
         self.root = root
-        self.root.title("Tarefas")
+        self.root.title("Minhas Tarefas")
         self.root.goemetry("400x600")
-        self.root.configure(bg = "fafafa")
+        self.root.configure(bg="fafafa")
         
         self.db = Database()
         self.db.criar_tabela()
         
-        self.db.criar_painel_principal()
+        self.db.painel_principal()
+    
+    def painel_principal(self):
+         
+         #limpar a tela principal
+        for widget in self.root.winfo_children():
+            widget.destroy()  
+        
+        tk.label(
+            self.root,
+            text="Minhas Tarefas",
+            font=("Arial", 20, "bold", "italic"),
+            bg="ffffff",
+            fg="333333"
+        ).pack(pady=20)   
+
     
